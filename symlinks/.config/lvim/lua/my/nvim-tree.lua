3178999f4721af42b979ac83884b4d7e64be7c7d
Rokas Brazdžionis <rokas.brazdzionis@gmail.com>
2022-07-01 17:51:58 +0300

    create angular schematics plugin for nvim-tree

--- symlinks/.config/lvim/lua/my/nvim-tree.lua
+++ symlinks/.config/lvim/lua/my/nvim-tree.lua
@@ -16,6 +16,5 @@ lvim.builtin.nvimtree.setup.view.mappings.list = {
   { key = "F", cb = "<cmd>lua require'lvim.core.nvimtree'.start_telescope('live_grep')<cr>" },
 
   { key = "A", cb = "<cmd>lua require('utils.toggle-nvim-tree').toggle_full_width()<cr>" },
-  -- TODO: make angular module / company generator
-  { key = "<C-h>", cb = "<cmd>lua require('utils.angular-schematics').run()<cr>" },
+  { key = "g", cb = "<cmd>lua require('utils.angular-schematics').run()<cr>" },
 }
--- symlinks/.config/lvim/lua/utils/angular-schematics.lua
+++ symlinks/.config/lvim/lua/utils/angular-schematics.lua
@@ -1,12 +1,28 @@
 local M = {}
 
+local function generate(schematic, nameWithFlags, directory)
+  local command = 'cd ' .. directory .. ' && ng generate ' .. schematic .. ' ' .. nameWithFlags
+  print(command)
+  os.execute(command)
+  require("nvim-tree.lib").refresh_tree();
+end
+
 function M.run()
   local node = require("nvim-tree.lib").get_node_at_cursor()
-  local abspath = node.link_to or node.absolute_path
+  local absolute_path = node.link_to or node.absolute_path
   local is_folder = node.open ~= nil
-  local basedir = is_folder and abspath or vim.fn.fnamemodify(abspath, ":h")
+  local basedir = is_folder and absolute_path or vim.fn.fnamemodify(absolute_path, ":h")
+  local relative_path = require('nvim-tree.utils').path_relative(basedir, require('nvim-tree.core').get_cwd())
 
-  print(basedir)
+  vim.ui.select({ 'module', 'component', 'directive', 'pipe', 'service', 'resolver', 'interceptor', 'guard', 'library',
+    'application' }, { prompt = 'What you would like to generate?' }, function(schematic)
+    if schematic == '' or schematic == nil then return end
+
+    vim.ui.input({ prompt = 'Enter the name [flags]: ' }, function(nameWithFlags)
+      if nameWithFlags == '' or nameWithFlags == nil then return end
+      generate(schematic, nameWithFlags, relative_path)
+    end)
+  end);
 end
 
 return M
