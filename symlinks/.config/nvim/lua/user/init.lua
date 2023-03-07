1099776ef8304489dc1b50b0b2ce61728e39f38c
Rokas Brazdžionis <rokas.brazdzionis@gmail.com>
2023-03-07 13:53:58 +0200

    enable eslint

--- symlinks/.config/nvim/lua/user/init.lua
+++ symlinks/.config/nvim/lua/user/init.lua
@@ -192,14 +192,13 @@ local config = {
           end
         }))
       end,
-      -- eslint_d = function()
-      --   require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with({
-      --     condition = function(utils)
-      --       return utils.root_has_file("package.json") or utils.root_has_file(".eslintrc.json") or
-      --           utils.root_has_file(".eslintrc.js")
-      --     end
-      --   }))
-      -- end,
+      eslint_d = function()
+        require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file(".eslintrc.json") or utils.root_has_file(".eslintrc.js")
+          end
+        }))
+      end,
     }
   },
 
