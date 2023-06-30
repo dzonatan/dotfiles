0ad8639094865a0759cab42bab0fde30654f2e57
Rokas Brazdžionis <rokas.brazdzionis@gmail.com>
2023-06-30 15:01:55 +0300

    turn off semantic tokens

--- symlinks/.config/nvim/lua/user/options.lua
+++ symlinks/.config/nvim/lua/user/options.lua
@@ -9,7 +9,8 @@ return {
     wrap = true,
     list = true,
   },
-  -- g = {
+  g = {
+    semantic_tokens_enabled = false, -- enable or disable LSP semantic tokens on startup
   --   mapleader = " ", -- sets vim.g.mapleader
   --   autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
   --   cmp_enabled = true, -- enable completion at start
@@ -17,7 +18,7 @@ return {
   --   diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
   --   icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
   --   ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-  -- },
+  },
 }
 -- If you need more control, you can use the function()...end notation
 -- return function(local_vim)
