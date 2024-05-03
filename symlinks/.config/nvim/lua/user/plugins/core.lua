cbc2e64211484e27e2ffdb6959717323c6746b19
Rokas Brazdžionis <rokas.brazdzionis@gmail.com>
2024-05-03 15:12:45 +0300

    mapping changes

--- symlinks/.config/nvim/lua/user/plugins/core.lua
+++ symlinks/.config/nvim/lua/user/plugins/core.lua
@@ -1,3 +1,24 @@
+local function check_json_key_exists(filename, key)
+  -- Open the file in read mode
+  local file = io.open(filename, "r")
+  if not file then
+    return false -- File doesn't exist or cannot be opened
+  end
+
+  -- Read the contents of the file
+  local content = file:read "*all"
+  file:close()
+
+  -- Parse the JSON content
+  local json_parsed, json = pcall(vim.fn.json_decode, content)
+  if not json_parsed or type(json) ~= "table" then
+    return false -- Invalid JSON format
+  end
+
+  -- Check if the key exists in the JSON object
+  return json[key] ~= nil
+end
+
 return {
   { "sainnhe/gruvbox-material" },
 
@@ -39,10 +60,19 @@ return {
         prettierd = function()
           require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
             condition = function(utils)
-              return utils.root_has_file ".prettierrc"
+              return check_json_key_exists(vim.fn.getcwd() .. "/package.json", "prettier")
+                or utils.root_has_file ".prettierrc"
                 or utils.root_has_file ".prettierrc.json"
-                or utils.root_has_file ".prettierrc.js"
                 or utils.root_has_file ".prettierrc.yml"
+                or utils.root_has_file ".prettierrc.yaml"
+                or utils.root_has_file ".prettierrc.json5"
+                or utils.root_has_file ".prettierrc.js"
+                or utils.root_has_file ".prettierrc.cjs"
+                or utils.root_has_file "prettier.config.js"
+                or utils.root_has_file ".prettierrc.mjs"
+                or utils.root_has_file "prettier.config.mjs"
+                or utils.root_has_file "prettier.config.cjs"
+                or utils.root_has_file ".prettierrc.toml"
             end,
           })
         end,
--- symlinks/.config/nvim/lua/user/plugins/treesitter.lua
+++ symlinks/.config/nvim/lua/user/plugins/treesitter.lua
@@ -7,8 +7,8 @@ return {
           keymaps = {
             ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
             ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-            ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-            ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+            -- ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+            -- ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
 
             ["am"] = { query = "@call.outer", desc = "Select outer part of a function call" },
             ["im"] = { query = "@call.inner", desc = "Select inner part of a function call" },
