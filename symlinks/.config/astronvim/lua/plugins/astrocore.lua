ed60d87c7ecea7444a17d4d78ef0b1b96db4f7df
Rokas Brazdžionis <rokas.brazdzionis@gmail.com>
2025-02-07 11:55:43 +0200

    format and add file path picker (Ctrl-f)

--- symlinks/.config/astronvim/lua/plugins/astrocore.lua
+++ symlinks/.config/astronvim/lua/plugins/astrocore.lua
@@ -19,17 +19,13 @@ end
 local toggle_quick_fix = function()
   local qf_exists = false
   for _, win in pairs(vim.fn.getwininfo()) do
-    if win["quickfix"] == 1 then
-      qf_exists = true
-    end
+    if win["quickfix"] == 1 then qf_exists = true end
   end
   if qf_exists == true then
     vim.cmd "cclose"
     return
   end
-  if not vim.tbl_isempty(vim.fn.getqflist()) then
-    vim.cmd "copen"
-  end
+  if not vim.tbl_isempty(vim.fn.getqflist()) then vim.cmd "copen" end
 end
 
 ---@type LazySpec
@@ -40,12 +36,10 @@ return {
     local nmaps = opts.mappings.n
     for lhs, key in pairs(nmaps) do
       -- remove all built-in mappings from `<leader>f` group so that we are not influenced by timeoutln
-      if lhs:match("^<Leader>f") then
-        nmaps[lhs] = nil
-      end
+      if lhs:match "^<Leader>f" then nmaps[lhs] = nil end
     end
 
-    local harpoon = require("harpoon")
+    local harpoon = require "harpoon"
     local telescopeConf = require("telescope.config").values
     local function toggle_telescope(harpoon_files)
       local file_paths = {}
@@ -53,22 +47,24 @@ return {
         table.insert(file_paths, item.value)
       end
 
-      require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-          results = file_paths,
-          entry_maker = function(entry)
-            return {
-              value = entry,
-              display = require("telescope.utils").path_tail(entry), -- Use smart path display
-              ordinal = entry,
-            }
-          end,
-        }),
-        previewer = telescopeConf.file_previewer({}),
-        sorter = telescopeConf.generic_sorter({}),
-        path_display = { "tail" },
-      }):find()
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table {
+            results = file_paths,
+            entry_maker = function(entry)
+              return {
+                value = entry,
+                display = require("telescope.utils").path_tail(entry), -- Use smart path display
+                ordinal = entry,
+              }
+            end,
+          },
+          previewer = telescopeConf.file_previewer {},
+          sorter = telescopeConf.generic_sorter {},
+          path_display = { "tail" },
+        })
+        :find()
     end
 
     return require("astrocore").extend_tbl(opts, {
@@ -109,61 +105,120 @@ return {
           -- harpoon
           ["<leader>m"] = { desc = "Harpoon" },
           ["<leader>ma"] = { function() harpoon:list():add() end, desc = "Harpoon add file" },
-          ["<leader>mm"] = { function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-            desc = "Harpoon quick menu" },
+          ["<leader>mm"] = {
+            function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+            desc = "Harpoon quick menu",
+          },
           ["<leader>j"] = { function() harpoon:list():next() end, desc = "Harpoon next file" },
           ["<leader>k"] = { function() harpoon:list():prev() end, desc = "Harpoon previous file" },
 
           -- search (new)
           ["<leader>s"] = { desc = "(S)earch" },
           ["<leader>sa"] = { function() require("telescope.builtin").git_files() end, desc = "Find files (git)" },
-          ["<leader>sn"] = { function() require("telescope.builtin").find_files { cwd = '%:p:h', hidden = true } end, desc = "Find neighboring files" },
-          ["<leader>sf"] = { function() require("telescope.builtin").find_files { hidden = true } end, desc = "Find all files" },
+          ["<leader>sn"] = {
+            function() require("telescope.builtin").find_files { cwd = "%:p:h", hidden = true } end,
+            desc = "Find neighboring files",
+          },
+          ["<leader>sf"] = {
+            function() require("telescope.builtin").find_files { hidden = true } end,
+            desc = "Find all files",
+          },
           ["<leader>sg"] = { function() require("telescope.builtin").live_grep() end, desc = "Live grep" },
           ["<leader>sr"] = { "<cmd>Telescope resume<CR>", desc = "Resume last search" },
           ["<leader>so"] = { function() require("telescope.builtin").oldfiles() end, desc = "Find history" },
-          ["<leader>sw"] = { function() require("telescope.builtin").grep_string() end, desc = "Find word under cursor" },
+          ["<leader>sw"] = {
+            function() require("telescope.builtin").grep_string() end,
+            desc = "Find word under cursor",
+          },
           ["<leader>st"] = { "<Cmd>TodoTelescope<CR>", desc = "Find TODOs" },
-          ["<leader>s/"] = { function() require("telescope.builtin").current_buffer_fuzzy_find() end, desc = "Find words in current buffer" },
-          ["<leader>ss"] = { function() require("telescope.builtin").lsp_document_symbols() end, desc = "Find document symbols" },
-          ["<leader>sS"] = { function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end, desc = "Find workspace symbols" },
-          ["<leader>sd"] = { function() require("telescope.builtin").diagnostics { bufnr = 0 } end, desc = "Find document symbols" },
+          ["<leader>s/"] = {
+            function() require("telescope.builtin").current_buffer_fuzzy_find() end,
+            desc = "Find words in current buffer",
+          },
+          ["<leader>ss"] = {
+            function() require("telescope.builtin").lsp_document_symbols() end,
+            desc = "Find document symbols",
+          },
+          ["<leader>sS"] = {
+            function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end,
+            desc = "Find workspace symbols",
+          },
+          ["<leader>sd"] = {
+            function() require("telescope.builtin").diagnostics { bufnr = 0 } end,
+            desc = "Find document symbols",
+          },
           ["<leader>sm"] = { function() toggle_telescope(harpoon:list()) end, desc = "Find in harpoon" },
 
-          ["<leader><leader>"] = { function() require("telescope.builtin").buffers() end, desc = "Search current buffers" },
+          ["<leader><leader>"] = {
+            function() require("telescope.builtin").buffers() end,
+            desc = "Search current buffers",
+          },
 
           ["<leader>o"] = { "<cmd>Neotree reveal<CR>", desc = "Toggle explorer (focus current)" },
-          
+
           ["<leader>lj"] = { function() vim.diagnostic.goto_next() end, desc = "Next diagnostic" },
-          
+
           ["<leader>a"] = { desc = "(A)rtificial Inteligence" },
           ["<leader>aa"] = { ":CopilotChatToggle<CR>", desc = "Toggle copilot chat" },
           ["<leader>ax"] = { ":CopilotChatReset<CR>", desc = "Reset copilot chat" },
           ["<leader>as"] = { ":CopilotChatStop<CR>", desc = "Stop copilot chat" },
           ["<leader>am"] = { ":CopilotChatModels<CR>", desc = "Select copilot chat model" },
-          ["<leader>ap"] = { function()
-            local actions = require("CopilotChat.actions")
-            require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-          end, desc = "Select copilot prompt" },
+          ["<leader>ap"] = {
+            function()
+              local actions = require "CopilotChat.actions"
+              require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+            end,
+            desc = "Select copilot prompt",
+          },
         },
         i = {
-          ["<C-CR>"] = { function() require('copilot.suggestion').accept() end, desc = "Accept Copilot suggestion" }
+          ["<C-CR>"] = { function() require("copilot.suggestion").accept() end, desc = "Accept Copilot suggestion" },
+          ["<C-f>"] = {
+            function()
+              local actions = require "telescope.actions"
+              local action_state = require "telescope.actions.state"
+              require("telescope.builtin").find_files {
+                hidden = true,
+                attach_mappings = function(prompot_bufnr, map)
+                  map("i", "<CR>", function() -- Override Enter in insert mode
+                    local entry = action_state.get_selected_entry()
+                    if entry then
+                      actions.close(prompot_bufnr)
+                      vim.api.nvim_put({ entry.path }, "", true, true)
+                    end
+                  end)
+                  map("n", "<CR>", function() -- Override Enter in normal mode
+                    local entry = action_state.get_selected_entry()
+                    if entry then
+                      actions.close(prompot_bufnr)
+                      vim.api.nvim_put({ entry.path }, "", true, true)
+                    end
+                  end)
+                  return true
+                end,
+              }
+            end,
+            desc = "Find and insert file path",
+          },
         },
         v = {
           --find by text
           ["<leader>f"] = { function() live_grep_visual() end, desc = "Live grep" },
           ["<leader>F"] = { function() live_grep_visual() end, desc = "Live grep" },
-          
+
           ["<leader>a"] = { desc = "(A)rtificial Inteligence" },
           ["<leader>aa"] = { ":CopilotChatOpen<CR>", desc = "Open copilot chat" },
-          ["<leader>ap"] = { function()
-            local actions = require("CopilotChat.actions")
-            require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-          end, desc = "Select copilot prompt" },
+          ["<leader>ap"] = {
+            function()
+              local actions = require "CopilotChat.actions"
+              require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+            end,
+            desc = "Select copilot prompt",
+          },
         },
         x = {
           -- do not override clipboard when pasting
-          ["p"] = "P"
+          ["p"] = "P",
         },
       },
     })
